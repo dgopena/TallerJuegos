b7f1e86afa1150d524e01a692a6d186e7011878d
dgopena <ddpena@uc.cl>
2016-06-29 19:10:40 -0400

    Agregadas seleccion de estrategias.

---
+++ Assets/Resources/Scripts/StrategyButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrategyButton : MonoBehaviour {
+    public string nextLayout = "";
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        string alt = transform.GetComponent<TextMesh>().text;
+        Log_writer.addLine("Estrategia elegida en item anterior:\n" + alt);
+        Application.LoadLevel(nextLayout);
+    }
+}
